ec61771703371daea65af18dd677c403da0181e7
Aagam Vakil <aagamvakil@hotmail.com>
2024-04-01 18:47:10 -0400

    Untrack files in .gitignore

--- creds.py
+++
@@ -1,9 +0,0 @@
-ALPACA_CONFIG = {
-    # Put your own Alpaca key here:
-    "API_KEY": "PKSR6L5CI5OF277XE8O3",
-    # Put your own Alpaca secret here:
-    "API_SECRET": "dbqVZnOlKS9EAAfQciOcumb9lUOfwtBjjcgQZoGq",
-    "PAPER": True,
-    # If you want to go live, you must change this. It is currently set for paper trading
-    # "ENDPOINT": "https://api.alpaca.markets"
-}
